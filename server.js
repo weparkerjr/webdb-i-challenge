9c1f61b97ea142814baf03a9e336a6be2766704b
william-parker <edwinious@gmail.com>
2019-08-13 11:19:21 -0400

    MVP + stretch

--- server.js
+++ server.js
@@ -1,9 +1,72 @@
 const express = require('express');
 
+const knex = require('knex');
 const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
 
-module.exports = server;
+// POST ACCOUNT
+server.post("/api/accounts", (req, res) => {
+  const account = req.body;
+  db('accounts')
+  .insert(account, 'id')
+  .then(posted => {
+    const response = posted[0];
+    res.status(200).json(response);
+  })
+  .catch(error => {
+    res.status(500).json(error)
+  })
+})
+
+// GET ACCOUNTS
+server.get("/api/accounts", (req, res) => {
+  db('accounts')
+  .select()
+  .then(accounts => {
+    res.status(200).json(accounts);
+  })
+  .catch(error => {
+    res.status(500).json(error)
+  })
+})
+
+// UPDATE ACCOUNT BY ID
+server.put("/api/accounts/:id", (req, res) => {
+  const newAccount = req.body;
+  const id = req.params.id;
+  db('accounts')
+  .where({id})
+  .update(newAccount)
+  .then(count => {
+    if(count > 0){
+       res.status(200).json({ message: `${count} account(s) updated`})
+    } else {
+      res.status(404).json({ message: 'not found' });
+    }
+  })
+  .catch(error => res.status(500).json(error));
+})
+
+// DELETE ACCOUNT BY ID
+server.delete('/api/accounts/:id', (req, res) => {
+  const id = req.params.id;
+db('accounts')
+.where({id})
+.del()
+.then(count => {
+  if(count > 0){
+    res.status(200).json({ message: `${count} account(s) deleted`})
+ } else {
+   res.status(404).json({ message: 'not found' });
+ }
+})
+.catch(error => {
+  res.status(500).json(error);
+})
+})
+
+
+module.exports = server;  
